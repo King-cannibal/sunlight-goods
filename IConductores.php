dcfdd667983723e95373a9fbc69af0143c81ea52
King-cannibal <94571105+King-cannibal@users.noreply.github.com>
2021-11-27 13:52:26 +0000

    e
    e

--- IConductores.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $Rfc = $_POST['Rfc'];
-    $Foto = $_POST['Foto'];
-
-     $SQL = "INSERT INTO Conductores VALUES ('$Rfc', '$Foto');";
-
-    $destdir = 'ImagenesPerfil/';   // path destination
-    $img=file_get_contents($Foto);
-    file_put_contents($destdir.substr($Foto, strrpos($Foto,'/')), $img);
